a2d61988b2ba31030b9b3342e1a2537bdbf407d5
HalleluyahBoy <eejimone@gmail.com>
2022-12-14 08:06:04 -0800

    Simple

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,19 @@
  */
 int print_sign(int n)
 {
-	int test;
-
 	if (n > 0)
 	{
-		test = 1;
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		test = 0;
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		test = -1;
 		_putchar('-');
+		return (-1);
 	}
-	return (test)
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,18 @@
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last_digit;
 
-	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	if (n < 0)
+	{
+		last_digit = (-1 * (n %100));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n  % 10);
+		_putchar (last_digit + '0');
+		return (last_digit)
+	}
 }
