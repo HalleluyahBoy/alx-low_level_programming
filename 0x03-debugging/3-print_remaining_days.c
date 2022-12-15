1b700d927599fa4211858016c06bcd2a6b8ae8de
HalleluyahBoy <eejimone@gmail.com>
2022-12-15 06:11:18 -0800

    Simple

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -30,6 +30,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n, 365 - day);
 		}
 	}
 }
