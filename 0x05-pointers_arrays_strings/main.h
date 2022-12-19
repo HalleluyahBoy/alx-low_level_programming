1c809a5bb35d9f14b9272c8d4242decded53e9c0
HalleluyahBoy <eejimone@gmail.com>
2022-12-19 09:05:24 -0800

    Simple

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,6 @@ void reset_to_98(int *n);
 	void print_rev(char *s);
 	void puts2(char *str);
 	void puts_half(char *str);
-	void print_array(int a, int n);
+	void print_array(int *a, int n);
 	char *_strcpy(char *dest, char *src);
 	void rev_string(char *s);
