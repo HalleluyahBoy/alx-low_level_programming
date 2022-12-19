0fec8b89c19f966d4ad1dc0cf5e2adc061c1489b
HalleluyahBoy <eejimone@gmail.com>
2022-12-19 07:44:52 -0800

    SImple

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,7 @@ void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index--)
+	for (index = 0; s[index] != '\0'; index++)
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
