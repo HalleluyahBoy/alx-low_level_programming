4a95fa631bf7ef34095b7888109c2d516b01a2b2
HalleluyahBoy <eejimone@gmail.com>
2022-12-22 03:50:39 -0800

    Simple

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
  * Return: A pointer to the changed string.
  */
 
-char *cap_string(char str)
+char *cap_string(char *str)
 {
 	int index = 0;
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -23,7 +23,7 @@ void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
-char *cap_string(char str);
+char *cap_string(char *);
 
 char *leet(char *);
 
