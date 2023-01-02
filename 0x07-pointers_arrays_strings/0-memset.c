c8f1b3f5b2d228f3ea52dcd4417a1c46023ec7eb
HalleluyahBoy <eejimone@gmail.com>
2023-01-02 13:39:47 -0800

    Simple

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,17 @@
-/*
- * File: 0-memset.c
- * Auth: not
- */
 #include "main.h"
-
 /**
- * _memset - Fills the first n bytes f the memory area
- * @s: A pointer to the memory area to be filled
- * @c: The character to fill the memory area with
- * @n: The number of bytes to be filled
- * description _memset: over there
- * Return: A pointer to the filled memory area @s
+ * _memset - fill the block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @n: number of bytes to be changed
+ * Return: changed array with new value of n bytes
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
-
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	int i = 0;
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
