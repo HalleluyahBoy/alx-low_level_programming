afbfc2e0a10ef2b2217f2be4d64372c43a76ec6a
HalleluyahBoy <eejimone@gmail.com>
2023-01-01 23:14:39 -0800

    Simple

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - prints the consecutive of sl that are in s2
+ * @s: source string
+ * @accept: searching
+ * Return: new string.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j) j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\n')
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
