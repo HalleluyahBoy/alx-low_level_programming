dd7fd50f1950806cca156e7d47ff6f579ff0cb0e
HalleluyahBoy <eejimone@gmail.com>
2023-01-04 23:16:08 -0800

    Simple

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,16 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: pointer to string
+ * _putchar - prints put the characters
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */
+	if (*s)
 	{
-		_putchar('\n);
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: me
+ */
+
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured
+ * Return: The length of th string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
