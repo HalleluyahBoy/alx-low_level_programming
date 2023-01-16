f0c45def589e8ab00b9744f0e1f36f1abdaf0afa
HalleluyahBoy <eejimone@gmail.com>
2023-01-16 07:23:47 -0800

    Simple

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ * Return: on success 1
+ * on error, -1 is returned and errno is set appropiately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
