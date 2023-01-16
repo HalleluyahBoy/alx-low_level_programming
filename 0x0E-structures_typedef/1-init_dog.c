405f7b55a49d625695e0f78b1390f75ab43a2d88
HalleluyahBoy <118768616+HalleluyahBoy@users.noreply.github.com>
2023-01-16 17:00:26 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: The owner of the dog
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d != NULL)
 	{
